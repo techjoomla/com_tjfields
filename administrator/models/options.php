808dd31a85c544da8b9b00cf265b40976dc857a6
Anupam Kumar <anupam_k@techjoomla.com>
2019-05-09 19:37:31 +0530

    Task #92 feat: com_api plugin for ucm

--- administrator/models/options.php
+++ administrator/models/options.php
@@ -1,10 +1,9 @@
 <?php
-<?php
 /**
  * @package    TjFields
  *
  * @author     Techjoomla <extensions@techjoomla.com>
- * @copyright  Copyright (c) 2009-2018 TechJoomla. All rights reserved.
+ * @copyright  Copyright (c) 2009-2019 TechJoomla. All rights reserved.
  * @license    GNU General Public License version 2 or later.
  */
 defined('_JEXEC') or die;
