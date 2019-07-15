bd575828a27cfe0fd2593be7593b0a54b2857304
“shraddha <shraddha_s@techjoomla.com>
2019-07-15 17:24:44 +0530

    Bug #143762 fix: add video layout in tjfield

---
+++ site/layouts/fields/video.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package	TJ-Fields
+ * @author	 TechJoomla <extensions@techjoomla.com>
+ * @copyright  Copyright (c) 2009-2019 TechJoomla. All rights reserved.
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Language\Text;
+
+if (!key_exists('field', $displayData) || !key_exists('fieldXml', $displayData))
+{
+	return;
+}
+
+$field = $displayData['field'];
+
+if ($field->value)
+{
+	// This is working for only form video field, For 
+	echo "<a href=" . $field->value . " target='_blank'>" . JText::_("COM_TJUCM_VIDEO_FIELD_VALUE") . "</a>";
+}
+else
+{
+	echo "-";
+}
