1cd2fa8439a1642b54ebc9432f435a4693fc3537
“shraddha <shraddha_s@techjoomla.com>
2019-07-16 18:34:20 +0530

    Bug #105512 fix: TJ ucm > type > field > calendar :: date format cant get change

---
+++ site/layouts/fields/calendar.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package	TJ-Fields
+ * @author	 TechJoomla <extensions@techjoomla.com>
+ * @copyright  Copyright (c) 2009-2019 TechJoomla. All rights reserved.
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+if (!key_exists('field', $displayData) || !key_exists('fieldXml', $displayData))
+{
+	return;
+}
+
+$field = $displayData['field'];
+$date = JFactory::getDate($field->value);
+$format = str_replace('%', '', $field->format);
+
+if ($field->value)
+{
+	// This is working for normal form calender field. Needs improvement for video field in subform
+	echo $date->format($format);
+}
+else
+{
+	echo "-";
+}
