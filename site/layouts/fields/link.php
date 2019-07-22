f0cf548b18bb826a4e2c55225d9a3e2f5242a1b1
“ankush_m” <ankush_m@techjoomla.com>
2019-07-22 18:23:55 +0530

    Task #147851 feat: Improve code structure to support same layout for ucmsubform and normal form

---
+++ site/layouts/fields/link.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package	TJ-Fields
+ * @author	 TechJoomla <extensions@techjoomla.com>
+ * @copyright  Copyright (c) 2009-2019 TechJoomla. All rights reserved.
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Language\Text;
+
+if (!key_exists('field', $displayData) || !key_exists('fieldXml', $displayData))
+{
+	return;
+}
+
+$field = $displayData['field'];
+
+if ($field->value)
+{
+	// This is working for normal form video field
+	$languageConstant = "COM_TJFIELDS_VIEW_FIELD_" . strtoupper($field->type);
+	echo "<a href=" . $field->value . " target='_blank'>" . JText::_($languageConstant) . "</a>";
+}
+else
+{
+	echo "-";
+}
