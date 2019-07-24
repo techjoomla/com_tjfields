d8a6eef59b08987d343f36e39eeb903d82dfb19f
poonam_b <poonam_b@techjoomla.com>
2019-07-24 17:21:44 +0530

    Issues #161 chore: Adding image layout to render an image

---
+++ site/layouts/fields/image.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package	TJ-Fields
+ * @author	 TechJoomla <extensions@techjoomla.com>
+ * @copyright  Copyright (c) 2009-2019 TechJoomla. All rights reserved.
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+if (!key_exists('field', $displayData) || !key_exists('fieldXml', $displayData))
+{
+	return;
+}
+
+JLoader::import('tjfields', JPATH_SITE . '/components/com_tjfields/helpers/');
+
+$xmlField = $displayData['fieldXml'];
+$field = $displayData['field'];
+$isSubFormField = (isset($displayData['isSubFormField'])) ? $displayData['isSubFormField'] : 0;
+$subFormFileFieldId = (isset($displayData['subFormFileFieldId'])) ? $displayData['subFormFileFieldId'] : 0;
+
+if ($field->value)
+{
+	JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_tjfields/tables');
+	$fieldsValueTable = JTable::getInstance('Fieldsvalue', 'TjfieldsTable');
+	$fieldsValueTable->load(array('value' => $field->value));
+
+	$extraParamArray = array();
+	$extraParamArray['id'] = $fieldsValueTable->id;
+
+	// Creating media link by check subform or not
+	if ($isSubFormField)
+	{
+		$extraParamArray['subFormFileFieldId'] = $subFormFileFieldId;
+	}
+
+	$tjFieldHelper = new TjfieldsHelper;
+	$mediaLink = $tjFieldHelper->getMediaUrl($field->value, $extraParamArray);
+	?>
+
+	<img src="<?php echo $mediaLink; ?>" height="<?php echo $field->element->attributes()->height;?>" width="<?php $field->element->attributes()->width;?>" />
+	<a href="<?php echo $mediaLink;?>" class="btn btn-success">
+	<?php echo JText::_("COM_TJFIELDS_FILE_DOWNLOAD");?></a>
+	<?php
+}
+else
+{
+	echo "-";
+}
