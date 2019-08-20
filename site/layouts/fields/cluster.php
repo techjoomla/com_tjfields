1b5fa4daf9efc07dfb19e4184fdf7a7304ef76f0
ankush-maherwal <ankush_m@techjoomla.com>
2019-08-20 16:59:40 +0530

    Bug #166 fix: Cluster Filters on UCM list view

---
+++ site/layouts/fields/cluster.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package	TJ-Fields
+ * @author	 TechJoomla <extensions@techjoomla.com>
+ * @copyright  Copyright (c) 2009-2019 TechJoomla. All rights reserved.
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+if (!key_exists('field', $displayData))
+{
+	return;
+}
+
+$field = $displayData['field'];
+
+if ($field->value)
+{
+	JLoader::import("/components/com_cluster/includes/cluster", JPATH_ADMINISTRATOR);
+	$clusterModel = ClusterFactory::model('Cluster', array('ignore_request' => true));
+	$cluster = $clusterModel->getItem($field->value);
+	echo $cluster->name;
+}
+else
+{
+	echo "-";
+}
