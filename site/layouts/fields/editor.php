2eb24a0bf0c24cf35acd7a086076c3a81e75f040
“ankush_m” <ankush_m@techjoomla.com>
2019-11-04 12:54:07 +0530

    Bug #218 fix: Editor field do not shows formated text in the list and details view

---
+++ site/layouts/fields/editor.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package	TJ-Fields
+ * @author	 TechJoomla <extensions@techjoomla.com>
+ * @copyright  Copyright (c) 2009-2019 TechJoomla. All rights reserved.
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+if (!key_exists('field', $displayData) || !key_exists('fieldXml', $displayData))
+{
+	return;
+}
+
+$xmlField = $displayData['fieldXml'];
+$field = $displayData['field'];
+
+if ($field->value)
+{
+	echo $field->value;
+}
