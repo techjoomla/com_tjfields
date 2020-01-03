8989e267ab8ea2dca0e688f74c60fd10c1b08475
ChavanAnkitaMahesh <ankita_c@techjoomla.com>
2020-01-03 18:45:15 +0530

    Bug100493 fix:Tool Tip DB column is varchar.. should be text

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `#__tjfields_fields` (
 	`required` varchar(255) NOT NULL,
 	`readonly` int(11) NOT NULL DEFAULT '0',
 	`created_by` int(11) NOT NULL,
-	`description` varchar(255) NOT NULL,
+	`description` text NOT NULL,
 	`js_function` text NOT NULL,
 	`validation_class` text NOT NULL,
 	`ordering` int(11) NOT NULL,
