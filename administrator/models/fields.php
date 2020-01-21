0e73837ddf77a73f6ad9d9987ab50f28f60568ee
Divya Chaudhari <divya_c@gmail.com>
2020-01-21 11:30:40 +0530

    Bug : Admin >> event fields :: search is not working for label of field

--- administrator/models/fields.php
+++ administrator/models/fields.php
@@ -149,7 +149,7 @@ class TjfieldsModelFields extends JModelList
 			else
 			{
 				$search = $db->Quote('%' . $db->escape($search, true) . '%');
-				$query->where('( a.type LIKE ' . $search . ' )');
+				$query->where('( a.label LIKE ' . $search . ' )');
 			}
 		}
 
