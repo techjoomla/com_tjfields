301b14db7462252f3c3fa4b1c99e4779f3b880c6
Tushar Shekokar <tushar_s@tekditechnologies.com>
2020-03-31 19:35:01 +0530

    Add api plugin for Country, City and Region

---
+++ plugins/api/tjfields/tjfields.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package     Tjfields
+ * @subpackage  com_tjfields
+ *
+ * @author      Techjoomla <extensions@techjoomla.com>
+ * @copyright   Copyright (C) 2009 - 2020 Techjoomla. All rights reserved.
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+defined('_JEXEC') or die( 'Restricted access');
+jimport('joomla.plugin.plugin');
+
+/**
+ * Base Class for api plugin
+ *
+ * @package     JGive
+ * @subpackage  component
+ * @since       __DEPLOY_VERSION__
+ */
+class PlgAPITjfields extends ApiPlugin
+{
+	/**
+	 * JGive api plugin to load com_api classes
+	 *
+	 * @param   string  $subject  The object to observe
+	 * @param   array   $config   An optional associative array of configuration settings.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function __construct($subject, $config = array())
+	{
+		parent::__construct($subject, $config = array());
+		ApiResource::addIncludePath(dirname(__FILE__) . '/tjfields');
+
+		// Load language files
+		$lang = JFactory::getLanguage();
+		$lang->load('plg_api_tjfields', JPATH_ADMINISTRATOR, '', true);
+		$lang->load('com_tjfields', JPATH_SITE, '', true);
+	}
+}
