bdee4926b1b9453d498085c5c5b9b492bc34f99c
“ankush_m” <ankush_m@techjoomla.com>
2020-04-09 16:32:03 +0530

    Task #158361 feat: Add functionality to compare value of number field with some other number field in the form

--- media/js/ui/file.js
+++ media/js/ui/file.js
@@ -1,7 +1,5 @@
 var tjFieldsFileField = {
-	previewMedia: function (url){
-
-	var tjUcmMediaUrl = 'https://docs.google.com/gview?url='+url+'&embedded=true';
+	previewMedia: function (tjUcmMediaUrl){
 		SqueezeBox.open(tjUcmMediaUrl ,{handler: "iframe", size: {x: window.innerWidth-50, y: window.innerHeight-50}});
 	}
 }
