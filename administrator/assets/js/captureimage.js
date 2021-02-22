0a5171a49c55b5c4a1a86044fe3f0f153bbe241d
ankush-maherwal <ankush_m@techjoomla.com>
2021-02-22 12:33:48 +0530

    Task #168716 feat: Capture field to capture and upload image on the s… (#321)
    * Task #168716 feat: Capture field to capture and upload image on the server
    
    * Task #168716 feat: Capture field to capture and upload image on the server
    
    * Task #168716 feat: Capture field to capture and upload image on the server

---
+++ administrator/assets/js/captureimage.js
@@ -0,0 +1,130 @@
+var userAgentIsMobile = false;
+var subFormFieldCount = 0;
+
+jQuery(document).ready(function() {
+	/* For mobile devices need to use a button to initialise camera as its not initialised on page load*/
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		userAgentIsMobile = true;
+	}
+
+	/* Initialise camera on page load by clicking a hidden button - the button is used in case of mobile devices as we cant activate camera on page load on mobile devices*/
+	jQuery.each(jQuery(document).find(jQuery("input[onClick^='set_camera(']")), function(key, value) {
+		if (jQuery('#' + jQuery(value).attr('id').replace('take_another', '_hasvalue')).val() == '0') {
+			jQuery(value).click();
+		}
+	});
+
+	/* Hide button to capture image on mobile device as this is done via mobile interface*/
+	jQuery.each(jQuery(document).find(jQuery("input[id^='_take_snapshot']")), function(key, value) {
+		if (userAgentIsMobile) {
+			document.getElementById(jQuery(value).attr('id')).style = 'display:none;';
+		}
+	});
+
+	/* Update the options of related field for new record of subform */
+	jQuery(document).on('subform-row-add', function(event, row) {
+		subFormFieldCount = jQuery(row).attr('data-group').replace(jQuery(row).attr('data-base-name'), "");
+		var likeName = 'jform_' + jQuery(row).attr('data-base-name') + '__' + jQuery(row).attr('data-group').substring(0, jQuery(row).attr('data-group').length - 7);
+
+		/* Update the id of elements used in field when a new group of field is added for subform */
+		jQuery.each(jQuery(row).find(jQuery("input[id^='" + likeName + "']")), function(key, value) {
+			jQuery(value).attr('id', jQuery(value).attr('id').replace("X__", subFormFieldCount + '__'));
+			jQuery(value).attr('name', jQuery(value).attr('id').replace("X__", subFormFieldCount + '__'));
+		});
+
+		/* Update the id of elements used in field when a new group of field is added for subform */
+		jQuery.each(jQuery(row).find(jQuery("div[id^='" + likeName + "']")), function(key, value) {
+			jQuery(value).attr('id', jQuery(value).attr('id').replace("X__", subFormFieldCount + '__'));
+			jQuery(value).attr('name', jQuery(value).attr('id').replace("X__", subFormFieldCount + '__'));
+		});
+
+		/* Initialise camera on page load by clicking a hidden button - the button is used in case of mobile devices as we cant activate camera on page load on mobile devices*/
+		jQuery.each(jQuery(row).find(jQuery("input[onClick^='set_camera(']")), function(key, value) {
+			if (jQuery('#' + jQuery(value).attr('id').replace('take_another', '_hasvalue')).val() == '0') {
+				jQuery(value).click();
+			}
+		});
+
+		/* Update the id of elements used in field when a new group of field is added for subform */
+		jQuery.each(jQuery(row).find(jQuery("input[onClick^='set_camera(']")), function(key, value) {
+			jQuery(value).attr('onClick', jQuery(value).attr('onClick').replace('X__', subFormFieldCount + '__'));
+		});
+
+		/* Update the id of elements used in field when a new group of field is added for subform */
+		jQuery.each(jQuery(row).find(jQuery("input[onClick^='take_snapshot(']")), function(key, value) {
+			jQuery(value).attr('onClick', jQuery(value).attr('onClick').replace('X__', subFormFieldCount + '__'));
+		});
+
+		/* Update the id of elements used in field when a new group of field is added for subform */
+		jQuery.each(jQuery(row).find(jQuery("input[onClick^='use_snapshot(']")), function(key, value) {
+			jQuery(value).attr('onClick', jQuery(value).attr('onClick').replace('X__', subFormFieldCount + '__'));
+		});
+
+		/* Hide button to capture image on mobile device as this is done via mobile interface*/
+		jQuery.each(jQuery(row).find(jQuery("input[id^='_take_snapshot']")), function(key, value) {
+			if (userAgentIsMobile) {
+				document.getElementById(jQuery(value).attr('id')).style = 'display:none;';
+			}
+		});
+	});
+});
+
+/* Method to take snapshot - used for non mobile devices*/
+function take_snapshot(thisId) {
+	Webcam.snap(function(data_uri) {
+		document.getElementById(thisId + '_capture_img').style = 'display:none';
+		document.getElementById(thisId + '_captured_img').style = '';
+		jQuery('#' + thisId + '_captured_img').find("img").attr("src", data_uri);
+		jQuery('#' + thisId).val(jQuery('#' + thisId + '_captured_img').find("img").attr("src")).trigger('change');
+		document.getElementById(thisId + '_take_snapshot').style = 'display:none;';
+		document.getElementById(thisId + '_take_another').style = '';
+	});
+}
+
+/* Method to take snapshot - used for mobile devices*/
+function use_snapshot(thisId) {
+	Webcam.snap(function(data_uri) {
+		document.getElementById(thisId + '_capture_img').style = 'display:none';
+		document.getElementById(thisId + '_captured_img').style = '';
+		jQuery('#' + thisId + '_captured_img').find("img").attr("src", data_uri);
+		Webcam.reset();
+
+		if (userAgentIsMobile) {
+			document.getElementById(thisId + '_take_snapshot').style = 'display:none;';
+			document.getElementById(thisId + '_use').style = 'display:none;';
+		}
+
+		jQuery('#' + thisId).val(jQuery('#' + thisId + '_captured_img').find("img").attr("src")).trigger('change');
+
+		document.getElementById(thisId + '_take_snapshot').style = 'display:none;';
+		document.getElementById(thisId + '_take_another').style = '';
+	});
+}
+
+/* Method to initialise and re-initialise camera - used for non mobile devices*/
+function set_camera(thisId) {
+	thisId = thisId.replace('X__', subFormFieldCount + '__');
+	document.getElementById(thisId + '_capture_img').style = '';
+	document.getElementById(thisId + '_captured_img').src = '';
+	jQuery('#' + thisId + '_captured_img').find("img").attr("src", '');
+	document.getElementById(thisId + '_captured_img').style = 'display:none';
+	document.getElementById(thisId + '_take_another').style = 'display:none;';
+	document.getElementById(thisId + '_take_snapshot').style = '';
+	document.getElementById(thisId + '_use').style = 'display:none;';
+
+	if (userAgentIsMobile) {
+		document.getElementById(thisId + '_take_snapshot').style = 'display:none;';
+		document.getElementById(thisId + '_use').style = '';
+	}
+
+	/* WebcamJs code to initialise camera - used for mobile devices*/
+	Webcam.set({
+		width: document.getElementById(thisId + '__camera_width').value,
+		height: document.getElementById(thisId + '__camera_height').value,
+		image_format: 'jpeg',
+		jpeg_quality: 180
+	});
+
+	/* WebcamJs code to attach camera outupt to a div on the page*/
+	Webcam.attach('#' + thisId + '_capture_img');
+}
---
+++ administrator/models/fields/captureimage.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * @package    Tjfields
+ * @author     Techjoomla <extensions@techjoomla.com>
+ * @copyright  Copyright (c) 2009-2021 TechJoomla. All rights reserved.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+JLoader::import("/techjoomla/media/storage/local", JPATH_LIBRARIES);
+
+defined('JPATH_PLATFORM') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Factory;
+
+JLoader::import('components.com_tjfields.models.fields.file', JPATH_ADMINISTRATOR);
+
+/**
+ * Form Field class for the image capture field.
+ * Provides an input field for files
+ *
+ * @link   http://www.w3.org/TR/html-markup/input.file.html#input.file
+ * @since  __DEPLOY_VERSION__
+ */
+class JFormFieldCaptureImage extends JFormFieldFile
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $type = 'Captureimage';
+
+	/**
+	 * The accepted Captureimage type list.
+	 *
+	 * @var    mixed
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $accept;
+
+	/**
+	 * Name of the layout being used to render the field
+	 *
+	 * @var    string
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $layout = 'joomla.form.field.file';
+
+	/**
+	 * Method to get certain otherwise inaccessible properties from the form field object.
+	 *
+	 * @param   string  $name  The property name for which to the the value.
+	 *
+	 * @return  mixed  The property value or null.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function __get($name)
+	{
+		require_once JPATH_SITE . '/components/com_tjfields/helpers/tjfields.php';
+
+		switch ($name)
+		{
+			case 'accept':
+				return $this->accept;
+		}
+
+		return parent::__get($name);
+	}
+
+	/**
+	 * Method to set certain otherwise inaccessible properties of the form field object.
+	 *
+	 * @param   string  $name   The property name for which to the the value.
+	 * @param   mixed   $value  The value of the property.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function __set($name, $value)
+	{
+		switch ($name)
+		{
+			case 'accept':
+				$this->accept = (string) $value;
+				break;
+			default:
+				parent::__set($name, $value);
+		}
+	}
+
+	/**
+	 * Method to attach a JForm object to the field.
+	 *
+	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+	 * @param   mixed             $value    The form field value to validate.
+	 * @param   string            $group    The field name group control value. This acts as as an array container for the field.
+	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                      full field name would end up being "bar[foo]".
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @see     JFormField::setup()
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function setup(SimpleXMLElement $element, $value, $group = null)
+	{
+		$return = parent::setup($element, $value, $group);
+
+		if ($return)
+		{
+			$this->accept = (string) $this->element['accept'];
+		}
+
+		return $return;
+	}
+
+	/**
+	 * Method to get the field input markup for the file field.
+	 * Field attributes allow specification of a maximum file size and a string
+	 * of accepted file extensions.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @note    The field does not include an upload mechanism.
+	 * @see     JFormFieldMedia
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected function getInput()
+	{
+		HTMLHelper::script('media/com_tjfields/vendors/webcamjs/webcam.min.js');
+		HTMLHelper::script('administrator/components/com_tjfields/assets/js/captureimage.min.js');
+
+		$layoutData = $this->getLayoutData();
+
+		$height = isset($layoutData['field']->element->attributes()->height) ? $layoutData['field']->element->attributes()->height : 240;
+		$width = isset($layoutData['field']->element->attributes()->width) ? $layoutData['field']->element->attributes()->width : 320;
+
+		Factory::getDocument()->addScriptDeclaration("
+			if (typeof userAgentIsMobile !== 'undefined')
+			{
+				var userAgentIsMobile = false;
+			}
+		");
+
+		$required = ($this->required) ? "required='required'" : "";
+		$hasValue = (!empty($this->value)) ? '1' : '0';
+		$html = "<input type='hidden' $required name='" . $this->name . "' id='" . $this->id . "'>";
+		$html .= "<input type='hidden' id='" . $this->id . "__hasvalue' value='" . $hasValue . "'>";
+		$html .= "<input type='hidden' id='" . $this->id . "__camera_height' value='" . $height . "'>";
+		$html .= "<input type='hidden' id='" . $this->id . "__camera_width' value='" . $width . "'>";
+
+		// Load backend language file
+		$lang = Factory::getLanguage();
+		$lang->load('com_tjfields', JPATH_SITE);
+
+		$displayTakePictureButton = (!empty($this->value)) ? 'style="display:none;"' : '';
+		$displayTakeAnotherButton = (!empty($this->value)) ? '' : 'style="display:none;"';
+		$displayCapturedImgDiv = (!empty($this->value)) ? 'style="display:none;"' : '';
+		$displayUsePictureButton = (!empty($this->value)) ? 'style="display:none;"' : '';
+
+		$html .= '<div id="' . $this->id . '_capture_img' . '"></div>';
+		$html .= '<div  ' . $displayCapturedImgDiv . ' id="' . $this->id . '_captured_img' . '"><img /></div>';
+		$html .= '<br>';
+		$html .= '<input ' . $displayTakePictureButton . ' type="button" id="' . $this->id . '_take_snapshot' . '" value="' . JText::_("COM_TJFIELDS_FILE_CAPTURE_IMAGE_TAKE_PHOTO") . '" onClick="' . "take_snapshot('" . $this->id . "')" . '">';
+		$html .= '<input ' . $displayUsePictureButton . ' type="button" id="' . $this->id . '_use' . '" value="' . JText::_("COM_TJFIELDS_FILE_USE_CAPTURED_IMAGE") . '" onClick="' . "use_snapshot('" . $this->id . "')" . '">';
+		$html .= '<input ' . $displayTakeAnotherButton . ' type="button" id="' . $this->id . '_take_another' . '" value="' . JText::_("COM_TJFIELDS_FILE_CAPTURE_IMAGE_TAKE_ANOTHER") . '" onClick="' . "set_camera('" . $this->id . "')" . '">';
+		$html .= '<br><br>';
+
+		if (!empty($layoutData["value"]))
+		{
+			$data = $this->buildData($layoutData);
+			$html .= '<div class="control-group">';
+			$html .= $data->html;
+
+			if (!empty($data->mediaLink))
+			{
+				$html .= $this->canDownloadFile($data, $layoutData);
+				$html .= $this->canDeleteFile($data, $layoutData);
+			}
+
+			$html .= '</div>';
+		}
+
+		return $html;
+	}
+}
