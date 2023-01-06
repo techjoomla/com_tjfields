a11d817066d75dd51a738cbcca2fb0146dfd75c0
Kishori Karale <kishori_k@techjoomla.com>
2023-01-06 16:52:42 +0530

    Feature #184273 feat: Conditional fields implementation

--- administrator/sql/updates/mysql/2.0.2.sql
+++ administrator/sql/updates/mysql/2.0.2.sql
@@ -2,13 +2,14 @@ ALTER TABLE `#__tjfields_fields_value` CHANGE `value` `value` mediumtext DEFAULT
 
 CREATE TABLE IF NOT EXISTS `#__tjfields_fields_conditions` (
 	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`state` tinyint(1) NOT NULL,
 	`show` int(11) NOT NULL,
 	`field_to_show` int(11) NOT NULL,
 	`condition_match` int(11) NOT NULL,
 	`field_on_show` int(11) NOT NULL,
-	`operator` int(11) NOT NULL,
-	`option` int(11) NOT NULL,
-	`state` tinyint(1) NOT NULL,
+	`condition` text CHARACTER NOT NULL,
+	`type_id` int NOT NULL,
+	`client` varchar(255) NOT NULL,
 	`created_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ,
 	`created_by` INT(10) NOT NULL DEFAULT '0' ,
 	`modified_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ,
